ed500032099240fe2563b6495602551e326f83d4
Felix A. Croes <felix@dworkin.nl>
2017-03-21 20:46:46 +0100

    Deserialize floats.

--- src/usr/Asheron/lib/Serialized.c
+++ src/usr/Asheron/lib/Serialized.c
@@ -6,7 +6,9 @@
 static mixed *deSerialize(string serialized, string format, varargs int number)
 {
     mixed *results;
-    int len, n, offset, i, j, x;
+    int len, n, offset, i, j;
+    int exponent, mantissa;
+    mixed x;
 
     if (number == 0) {
 	number = 1;	/* default to 1 struct */
@@ -45,6 +47,28 @@ static mixed *deSerialize(string serialized, string format, varargs int number)
 		 */
 		x = serialized[offset];
 		offset++;
+		break;
+
+	    case 'f':
+		/*
+		 * 4 byte IEEE float, little endian
+		 */
+		exponent = ((serialized[offset + 3] & 0x7f) << 1) +
+			   (serialized[offset + 2] >> 7);
+		mantissa = ((serialized[offset + 2] & 0x7f) << 16) +
+			   (serialized[offset + 1] << 8) + serialized[offset];
+		if (exponent == 0) {
+		    if (mantissa == 0) {
+			x = 0.0;
+			break;
+		    }
+		} else {
+		    mantissa += 0x800000;	/* implied high bit */
+		}
+		if (serialized[offset + 3] & 0x80) {
+		    mantissa = -mantissa;	/* negative */
+		}
+		x = ldexp((float) mantissa, exponent - 127 - 23);
 		break;
 
 	    default:
