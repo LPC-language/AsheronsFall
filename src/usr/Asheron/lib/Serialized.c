c313949a12d896eede7edc4867a4bc980031aba5
Felix A. Croes <felix@dworkin.nl>
2017-08-03 23:06:31 +0200

    Add DD format (int + float) to serialization.

--- src/usr/Asheron/lib/Serialized.c
+++ src/usr/Asheron/lib/Serialized.c
@@ -2,6 +2,116 @@
 # define DBL_BIAS	1023
 
 /*
+ * serialize a double precision floating point number
+ */
+private string serializeDouble(float flt)
+{
+    string str;
+    float mantissa, bits;
+    int exponent, num;
+
+    str = "\0\0\0\0\0\0\0\0";
+    if (flt != 0.0) {
+	({ mantissa, exponent }) = frexp(flt);
+	exponent += DBL_BIAS - 1;
+	if (exponent > 0) {
+	    num = 29;
+	} else {
+	    num = exponent + 28;
+	    if (num <= 0) {
+		return str;
+	    }
+	    exponent = 0;	/* denormalized */
+	}
+	if (mantissa < 0.0) {
+	    /* negative */
+	    mantissa = -mantissa;
+	    str[7] = 0x80 + (exponent >> 4);
+	} else {
+	    str[7] = exponent >> 4;
+	}
+	mantissa = ldexp(mantissa, num);
+	bits = floor(mantissa);
+	num = (int) bits;
+	str[6] = ((num >> 24) & 0x0f) + (exponent << 4);
+	str[5] = num >> 16;
+	str[4] = num >> 8;
+	str[3] = num;
+	num = (int) floor(ldexp(mantissa - bits, 24));
+	str[2] = num >> 16;
+	str[1] = num >> 8;
+	str[0] = num;
+    }
+
+    return str;
+}
+
+/*
+ * serialize mantissa bits in a double precision floating point number
+ */
+private string serializeBits(string str, int bits, int exp, int space,
+			     int offset)
+{
+    if (exp < space) {
+	do {
+	    str[offset++] |= bits << (space - exp);
+	    space -= 8;
+	} while (space > 32);
+	if (exp < space) {
+	    bits <<= space - exp;
+	}
+    }
+    if (exp > space) {
+	bits >>= exp - space;
+    }
+
+    while (bits != 0) {
+	str[offset++] |= bits;
+	bits >>= 8;
+    }
+    return str;
+}
+
+/*
+ * serialize a "date" (an integer and a floating point fragment)
+ */
+private string serializeDate(int secs, float msecs)
+{
+    string str;
+    float mantissa;
+    int exponent, num;
+
+    if (secs == 0) {
+	return serializeDouble(msecs);
+    } else if (msecs == 0.0) {
+	return serializeDouble((float) secs);
+    }
+
+    str = "\0\0\0\0\0\0\0\0";
+    ({ mantissa, exponent }) = frexp((float) secs);
+    num = exponent - 1;
+    exponent += DBL_BIAS - 1;
+    str[7] = exponent >> 4;
+    str[6] = exponent << 4;
+    if (secs < 0) {
+	secs = -secs;
+	msecs = -msecs;
+	str[7] |= 0x80;
+    }
+    str = serializeBits(str, secs ^ (1 << num), num, 36, 2);
+
+    ({ mantissa, exponent }) = frexp(msecs);
+    num = 52 - num + exponent;
+    if (num > 0) {
+	exponent = (num > 31) ? 31 : num;
+	str = serializeBits(str, (int) floor(ldexp(mantissa, exponent)),
+			    exponent, num, 0);
+    }
+
+    return str;
+}
+
+/*
  * Serialize a number of arguments, given a format string.  Return a string
  * with the serialized data.
  */
@@ -9,7 +119,7 @@ static string serialize(string format, varargs mixed args...)
 {
     int len, i, n, offset, num, exponent;
     string *result, str;
-    float mantissa, bits;
+    float mantissa;
 
     len = strlen(format);
     result = allocate(len);
@@ -37,7 +147,7 @@ static string serialize(string format, varargs mixed args...)
 	    offset += 4;
 	    break;
 
-	case 'p':
+	case 'I':
 	    /*
 	     * packed 31 bit integer, not quite little endian
 	     */
@@ -129,39 +239,21 @@ static string serialize(string format, varargs mixed args...)
 	    /*
 	     * 8 byte IEEE double, little endian
 	     */
-	    str = "\0\0\0\0\0\0\0\0";
+	    str = serializeDouble(args[n++]);
 	    offset += 8;
-	    ({ mantissa, exponent }) = frexp(args[n++]);
-	    if (mantissa != 0.0) {
-		exponent += DBL_BIAS - 1;
-		if (exponent > 0) {
-		    num = 29;
-		} else {
-		    num = exponent + 28;
-		    if (num <= 0) {
-			break;
-		    }
-		    exponent = 0;	/* denormalized */
-		}
-		if (mantissa < 0.0) {
-		    /* negative */
-		    mantissa = -mantissa;
-		    str[7] = 0x80 + (exponent >> 4);
-		} else {
-		    str[7] = exponent >> 4;
-		}
-		mantissa = ldexp(mantissa, num);
-		bits = floor(mantissa);
-		num = (int) bits;
-		str[6] = ((num >> 24) & 0x0f) + (exponent << 4);
-		str[5] = num >> 16;
-		str[4] = num >> 8;
-		str[3] = num;
-		num = (int) floor(ldexp(mantissa - bits, 24));
-		str[2] = num >> 16;
-		str[1] = num >> 8;
-		str[0] = num;
+	    break;
+
+	case 'D':	/* DD */
+	    /*
+	     * (int, float) as 8 byte IEEE double, little endian
+	     */
+	    result[i] = "";
+	    if (format[++i] != 'D') {
+		error("Unknown format 'D" + format[i .. i] + "'");
 	    }
+	    num = args[n++];
+	    str = serializeDate(num, args[n++]);
+	    offset += 8;
 	    break;
 
 	default:
@@ -174,6 +266,92 @@ static string serialize(string format, varargs mixed args...)
 }
 
 /*
+ * deserialize a double precision floating point number
+ */
+private float deSerializeDouble(string serialized)
+{
+    int exponent, mantissa24, mantissa28;
+    float x;
+
+    exponent = ((serialized[7] & 0x7f) << 4) +
+	       (serialized[6] >> 4);
+    mantissa28 = ((serialized[6] & 0x0f) << 24) +
+		 (serialized[5] << 16) +
+		 (serialized[4] << 8) +
+		 serialized[3];
+    mantissa24 = (serialized[2] << 16) +
+		 (serialized[1] << 8) +
+		 serialized[0];
+    if (exponent == 0) {
+	if ((mantissa24 | mantissa28) == 0) {
+	    return 0.0;
+	}
+	exponent = 1;		/* denormalized */
+    } else {
+	mantissa28 += 0x10000000;	/* implied high bit */
+    }
+    x = ldexp((float) mantissa28, exponent - DBL_BIAS - 28) +
+	ldexp((float) mantissa24, exponent - DBL_BIAS - 52);
+    if (serialized[7] & 0x80) {
+	x = -x;	/* negative */
+    }
+
+    return x;
+}
+
+/*
+ * extract mantissa bits from a double precision floating point number
+ */
+private int deSerializeBits(string serialized, int bits, int offset)
+{
+    int x, i, n;
+
+    x = 0;
+    i = (offset >> 3) + 1;
+    for (n = bits + ((offset + 7) & ~0x7) - offset; n >= 8; n -= 8) {
+	x = (x << 8) | serialized[--i];
+    }
+    if (n != 0) {
+	x = (x << n) | (serialized[--i] >> (8 - n));
+    }
+    return x & ~(0xffffffff << bits);
+}
+
+/*
+ * deserialize a "date" from a double precision floating point number
+ */
+private mixed *deSerializeDate(string serialized)
+{
+    int exponent, offset, x;
+    float y;
+
+    exponent = ((serialized[7] & 0x7f) << 4) +
+	       (serialized[6] >> 4);
+    if (exponent == 0) {
+	return ({ 0, 0.0 });
+    }
+    exponent -= DBL_BIAS;
+    if (exponent < 0) {
+	return ({ 0, deSerializeDouble(serialized) });
+    }
+    if (exponent > 30) {
+	error("DD format number too large");
+    }
+
+    offset = 52;
+    x = (exponent != 0) ?
+	 deSerializeBits(serialized, exponent, offset) | (1 << exponent) : 1;
+    offset -= exponent;
+    exponent = (offset <= 31) ? offset : 31;
+    y = ldexp((float) deSerializeBits(serialized, exponent, offset), -exponent);
+    if (serialized[7] & 0x80) {
+	x = -x;
+	y = -y;
+    }
+    return ({ x, y });
+}
+
+/*
  * Deserialize a string, based on a format description.  Returns an array
  * containing the remaining unserialized input, and the results.
  */
@@ -181,7 +359,7 @@ static mixed *deSerialize(string serialized, string format, varargs int number)
 {
     mixed *results;
     int len, n, offset, i, j;
-    int exponent, mantissa24, mantissa28;
+    int exponent, mantissa;
     mixed x;
 
     if (number == 0) {
@@ -213,7 +391,7 @@ static mixed *deSerialize(string serialized, string format, varargs int number)
 		offset += 4;
 		break;
 
-	    case 'p':
+	    case 'I':
 		/*
 		 * packed 31 bit integer, not quite little endian
 		 */
@@ -263,20 +441,20 @@ static mixed *deSerialize(string serialized, string format, varargs int number)
 		 */
 		exponent = ((serialized[offset + 3] & 0x7f) << 1) +
 			   (serialized[offset + 2] >> 7);
-		mantissa24 = ((serialized[offset + 2] & 0x7f) << 16) +
-			     (serialized[offset + 1] << 8) +
-			     serialized[offset];
+		mantissa = ((serialized[offset + 2] & 0x7f) << 16) +
+			   (serialized[offset + 1] << 8) +
+			   serialized[offset];
 		if (exponent == 0) {
-		    if (mantissa24 == 0) {
+		    if (mantissa == 0) {
 			x = 0.0;
 			offset += 4;
 			break;
 		    }
 		    exponent = 1;		/* denormalized */
 		} else {
-		    mantissa24 += 0x800000;	/* implied high bit */
+		    mantissa += 0x800000;	/* implied high bit */
 		}
-		x = ldexp((float) mantissa24, exponent - FLT_BIAS - 23);
+		x = ldexp((float) mantissa, exponent - FLT_BIAS - 23);
 		if (serialized[offset + 3] & 0x80) {
 		    x = -x;	/* negative */
 		}
@@ -287,30 +465,19 @@ static mixed *deSerialize(string serialized, string format, varargs int number)
 		/*
 		 * 8 byte IEEE float, little endian
 		 */
-		exponent = ((serialized[offset + 7] & 0x7f) << 4) +
-			   (serialized[offset + 6] >> 4);
-		mantissa28 = ((serialized[offset + 6] & 0x0f) << 24) +
-			     (serialized[offset + 5] << 16) +
-			     (serialized[offset + 4] << 8) +
-			     serialized[offset + 3];
-		mantissa24 = (serialized[offset + 2] << 16) +
-			     (serialized[offset + 1] << 8) +
-			     serialized[offset];
-		if (exponent == 0) {
-		    if (mantissa24 | mantissa28 == 0) {
-			x = 0.0;
-			offset += 8;
-			break;
-		    }
-		    exponent = 1;		/* denormalized */
-		} else {
-		    mantissa28 += 0x10000000;	/* implied high bit */
+		x = deSerializeDouble(serialized[offset .. offset + 7]);
+		offset += 8;
+		break;
+
+	    case 'D':	/* DD */
+		/*
+		 * 8 byte IEEE float, little endian as (int, float)
+		 */
+		if (format[++j] != 'D') {
+		    error("Unknown format 'D" + format[j .. j] + "'");
 		}
-		x = ldexp((float) mantissa28, exponent - DBL_BIAS - 28) +
-		    ldexp((float) mantissa24, exponent - DBL_BIAS - 52);
-		if (serialized[offset + 7] & 0x80) {
-		    x = -x;	/* negative */
-		}
+		({ results[++n], x }) =
+			    deSerializeDate(serialized[offset .. offset + 7]);
 		offset += 8;
 		break;
 
