a750af7334ebf596b8e71fc36b48578591b6949f
Felix A. Croes <felix@dworkin.nl>
2017-10-20 23:16:54 +0200

    Gather vitals and attributes in arrays.

--- src/creature/lib/Creature.c
+++ src/creature/lib/Creature.c
@@ -9,15 +9,8 @@ inherit PhysicalObject;
  */
 
 private int id;
-private int health, healthMax;
-private int stamina, staminaMax;
-private int mana, manaMax;
-private int strength;
-private int endurance;
-private int coordination;
-private int quickness;
-private int focus;
-private int self;
+private int *vitals;
+private int *attributes;
 private int *skills;
 private int level;
 private float experience;
@@ -35,8 +28,7 @@ static void create(int class, string name, int *attributes, int *skills)
     }
     id = (id << 16) + OBJECT_ID_PERSISTENT;
 
-    ({ strength, endurance, coordination, quickness, focus, self }) =
-								    attributes;
+    ::attributes = attributes;
     ::skills = skills;
     inventory = ({ new Container(102) });
 }
